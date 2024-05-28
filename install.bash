47fb2aac6f98854ebf82b61760fd61f99987fd72
George Nurser <agn@noc.ac.uk>
2024-05-28 10:13:34 +0100

    install.bash now installs netcdf and numba

--- install.bash
+++ install.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function run_build(){
+    mamba install netcdf4 numba
     meson setup builddir --python.install-env auto
     meson compile -C builddir
     meson install -C builddir
