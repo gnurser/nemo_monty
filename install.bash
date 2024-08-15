dfa3674e6613a51ad5cdbe421ad83b16e54986fa
George Nurser <agn@noc.ac.uk>
2024-08-15 11:03:17 +0100

    install.bash only consults mamba to ensure netcdf4 and numba if they're not installed

--- install.bash
+++ install.bash
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 function run_build(){
-    mamba install netcdf4 numba
+if python -c "import numba" > /dev/null 2>&1; then
+    if python -c "import netcdf4" > /dev/null 2>&1; then
+	:
+    else
+	mamba install netcdf4
+    fi
+else
+    if python -c "import netcdf4" > /dev/null 2>&1; then
+	mamba install numba
+    else
+	mamba install netcdf4 numba
+    fi
+fi
+
     meson setup builddir --python.install-env auto
     meson compile -C builddir
     meson install -C builddir
     echo building
-    }
-    if python -c "import nemo_eos" > /dev/null 2>&1; then
-	run_build
-    else
-	echo nemo_eos not installed in this environment
-    fi
+}
+
+if python -c "import nemo_eos" > /dev/null 2>&1; then
+    run_build
+else
+    echo nemo_eos not installed in this environment
+fi
